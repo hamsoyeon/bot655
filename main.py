3d5e74008aa94ec920db8d97a60acc8547afbb2a
hamsoyeon <chair3203@naver.com>
2021-03-06 17:28:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ async def on_ready():
     print(client.user.id)
     print("ready")
     game=discord.Game("테스트봇")
-    await client.change_presence(status=discord.Status.offline, activity=game)
+    await client.change_presence(status=discord.Status.online, activity=game)
 
 
 @client.event
@@ -17,4 +17,4 @@ async def on_message(message):
         await message.channel.send("우주에 있는거 같아. 반가워.")
 
 access_token=os.environ["BOT_TOKEN"]
-client.run(access_token);
+client.run(access_token)
