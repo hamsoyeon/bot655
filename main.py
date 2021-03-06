30a153ee4d5d7a20c7edcc1e0c0ea917b0b5b094
hamsoyeon <62364842+hamsoyeon@users.noreply.github.com>
2021-03-06 16:47:09 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(client.user.id)
+    print("ready")
+    game=discord.Game("테스트봇")
+    await client.change_presence(status=discord.Status.offline, activity=game)
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("안녕"):
+        await message.channel.send("우주에 있는거 같아. 반가워.")
+
+
+client.run("ODE3MzM1ODU2NTEwNzk1ODE2.YEIBVQ.F2R0_c6PfURBVJ8WjJI2QzNC604");
